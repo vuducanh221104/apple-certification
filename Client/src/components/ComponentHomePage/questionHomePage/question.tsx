8d195dd1524989df5767c8df7f2b022bc3526f57
das110722 <phuocvu.hcmc@gmail.com>
2025-04-02 16:57:47 +0700

    update questionHomePage

--- Client/src/components/ComponentHomePage/questionHomePage/question.tsx
+++ Client/src/components/ComponentHomePage/questionHomePage/question.tsx
@@ -7,39 +7,9 @@ import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from 'swiper/modules';
 import styles from "./question.module.scss";
+import {slides} from "@/service/mockApi"
 
-const slides = [
-  {
-    image: "https://www.unkeyapp.com/svg/setting.svg",
-    title: "Cài đặt ứng dụng VIP với một chạm",
-    content: "Tận hưởng hàng ngàn ứng dụng modded, VIP mà không cần jailbreak, thao tác đơn giản chỉ với một lần chạm.",
-  },
-  {
-    image: "https://www.unkeyapp.com/svg/api.svg",
-    title: "Tích hợp API cho lập trình viên",
-    content: "Tích hợp API cho phép lập trình viên truy cập và quản lý ứng dụng, giúp phát triển và quản lý các dự án dễ dàng hơn.",
-  },
-  {
-    image: "https://www.unkeyapp.com/svg/security.svg",
-    title: "Không cần jailbreak có thể giữ nguyên bảo hành",
-    content: "Không can thiệp hệ thống, đảm bảo an toàn và vẫn được bảo hành chính hãng.",
-  },
-  {
-    image: "https://www.unkeyapp.com/svg/security.svg",
-    title: "Không cần jailbreak có thể giữ nguyên bảo hành",
-    content: "Không can thiệp hệ thống, đảm bảo an toàn và vẫn được bảo hành chính hãng.",
-  },
-  {
-    image: "https://www.unkeyapp.com/svg/security.svg",
-    title: "Không cần jailbreak có thể giữ nguyên bảo hành",
-    content: "Không can thiệp hệ thống, đảm bảo an toàn và vẫn được bảo hành chính hãng.",
-  },
-  {
-    image: "https://www.unkeyapp.com/svg/security.svg",
-    title: "Không cần jailbreak có thể giữ nguyên bảo hành",
-    content: "Không can thiệp hệ thống, đảm bảo an toàn và vẫn được bảo hành chính hãng.",
-  },
-];
+
 
 const QuestionApp: React.FC = () => {
   return (
@@ -63,7 +33,7 @@ const QuestionApp: React.FC = () => {
         {slides.map(({ image, title, content }, index) => (
           <SwiperSlide key={index} className={styles.slide}>
             <div className={styles.slideContent}>
-              <Image src={image} alt={title} width={32} height={32} className={styles.slideImage} />
+              <Image src={image.trim()} alt={title} width={32} height={32} className={styles.slideImage} />
               
               <h2 className={styles.slideTitle}>{title}</h2>
               
--- Client/src/service/mockApi.ts
+++ Client/src/service/mockApi.ts
@@ -132,3 +132,53 @@ import { AppComponents } from '@/types/client';
 
 
 // AppSingleList
+
+
+// questionHomePage
+export const slides = [
+    {
+      image: "https://www.unkeyapp.com/svg/setting.svg",
+      title: "Cài đặt ứng dụng VIP với một chạm",
+      content: "Tận hưởng hàng ngàn ứng dụng modded, VIP mà không cần jailbreak, thao tác đơn giản chỉ với một lần chạm.",
+    },
+    {
+      image: "https://www.unkeyapp.com/svg/api.svg",
+      title: "Tích hợp API cho lập trình viên",
+      content: "Tích hợp API cho phép lập trình viên truy cập và quản lý ứng dụng, giúp phát triển và quản lý các dự án dễ dàng hơn.",
+    },
+    {
+      image: "https://www.unkeyapp.com/svg/b_check.svg",
+      title: "Không cần jailbreak có thể giữ nguyên bảo hành",
+      content: "Không can thiệp hệ thống, đảm bảo an toàn và vẫn được bảo hành chính hãng.",
+    },
+    {
+      image: "https://www.unkeyapp.com/svg/coppy.svg",
+      title: "Nhân bản ứng dụng không giới hạn",
+      content: "Khả năng nhân bản không giới hạn các ứng dụng yêu thích trên iPhone hoặc iPad, sử dụng nhiều tài khoản cùng lúc mà không gặp bất kỳ trở ngại nào.",
+    },
+    {
+      image: "https://www.unkeyapp.com/svg/apple.svg",
+      title: "Hỗ trợ trên tất cả các phiên bản iOS",
+      content: "Tương thích hoàn hảo với tất cả các phiên bản iOS, từ iOS cũ cho đến iOS mới nhất, đảm bảo không giới hạn trong việc sử dụng ứng dụng.",
+    },
+    {
+      image: "https://www.unkeyapp.com/svg/applicate.svg",
+      title: "Truy cập kho ứng dụng không giới hạn",
+      content: "Tự do truy cập và sử dụng một thư viện ứng dụng phong phú, phù hợp với mọi nhu cầu sử dụng của bạn.",
+    },
+    {
+      image: "https://www.unkeyapp.com/svg/security.svg",
+      title: "Bảo mật cao, vượt trội so với Jailbreak",
+      content: "Bảo mật vượt trội hơn việc jailbreak thiết bị, không can thiệp vào hệ thống và hoàn toàn an toàn cho dữ liệu của bạn.",
+    },
+    {
+      image: "https://www.unkeyapp.com/svg/fast.svg",
+      title: "Thời gian kích hoạt nhanh chóng",
+      content: "Đăng ký và sử dụng ứng dụng ngay lập tức sau khi thanh toán, giúp bạn tiết kiệm thời gian và tận hưởng trải nghiệm liền mạch.",
+    },
+    {
+      image: "	https://www.unkeyapp.com/svg/note.svg",
+      title: "Gói dịch vụ đa dạng, phù hợp với mọi nhu cầu",
+      content: "Các gói dịch vụ linh hoạt từ cơ bản đến cao cấp, phù hợp với mọi nhu cầu của cá nhân đến doanh nghiệp.",
+    },
+  ];
